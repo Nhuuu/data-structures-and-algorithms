c2cd2c5cf980c7285051abae97db1d1bf1aec455
Nhu Trinh <nqtrinh87@gmail.com>
2019-08-05 20:31:04 -0700

    challenge done

--- code401challenges/ArrayReverse.java
+++ code401challenges/ArrayReverse.java
@@ -2,10 +2,20 @@ import java.util.Arrays;
 
 public class ArrayReverse {
   public static void main(String[] args){
-    int [] toBeReversed = new int[]{1,2,3,4,5};
+    int[] toBeReversed = new int[]{1,2,3,4,5};
     System.out.println(Arrays.toString(reverseArray(toBeReversed)));
   }
   public static int[] reverseArray(int[] arr){
-    return arr;
+    if (arr.length <= 1) {
+      return arr;
+    } else {
+      int[] newArr = new int[arr.length];
+      int index = 0;
+      for ( int i = arr.length - 1; i >= 0; i-- ){
+        newArr[index] = arr[i];
+        index++;
+      }
+      return newArr;
+    }
   }
 }
