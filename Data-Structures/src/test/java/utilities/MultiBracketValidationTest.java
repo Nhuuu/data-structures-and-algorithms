bb156b08606d67544dafe0bb2342ae64749c549a
Nhu Trinh <nqtrinh87@gmail.com>
2019-08-25 17:00:20 -0700

    finished challenge

--- Data-Structures/src/test/java/utilities/MultiBracketValidationTest.java
+++ Data-Structures/src/test/java/utilities/MultiBracketValidationTest.java
@@ -8,41 +8,37 @@ public class MultiBracketValidationTest {
 
   @Test
   public void multiBracketValidation_1() {
-
+    String b = "{}";
+    assertTrue(MultiBracketValidation.validate(b));
   }
 
   @Test
   public void multiBracketValidation_2() {
-
+    String b = "{}(){}";
+    assertTrue(MultiBracketValidation.validate(b));
   }
 
   @Test
   public void multiBracketValidation_3() {
-
+    String b = "(){}[[]]";
+    assertTrue(MultiBracketValidation.validate(b));
   }
 
   @Test
   public void multiBracketValidation_4() {
-
+    String b = "[({}]";
+    assertFalse(MultiBracketValidation.validate(b));
   }
 
   @Test
   public void multiBracketValidation_5() {
-
+    String b = "(](";
+    assertFalse(MultiBracketValidation.validate(b));
   }
 
   @Test
   public void multiBracketValidation_6() {
-
-  }
-
-  @Test
-  public void multiBracketValidation_7() {
-
-  }
-
-  @Test
-  public void multiBracketValidation_8() {
-
+    String b = "{(})";
+    assertFalse(MultiBracketValidation.validate(b));
   }
 }
